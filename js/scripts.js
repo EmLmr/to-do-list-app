95a17a5893a6ced92c9e9a788bdd06224c564922
EmelineLmr <emeline.coding@gmail.com>
2021-07-09 11:10:08 +0200

    implemented function to add item to list

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+function newItem() {
+
+  //1. Adding a new item to the list of items:
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  let text = document.createTextNode(inputValue);
+
+  li.append(inputValue);
+
+    // Conditional to make sure a list entry was made.
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    let list = $('#list');
+    list.append(li);
+  }
+
+}
